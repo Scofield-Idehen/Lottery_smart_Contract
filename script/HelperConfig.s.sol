bed623dab39c680b9ae407b6d1092e9dd212e77b
scofield <scofields109@gmail.com>
2024-11-27 05:24:15 +0100

    learning

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -21,10 +21,10 @@ contract HelperConfig is Script{
 
     function getSapolia() public view returns (Networkconfig memory){
         return NetworkConfig({
-            entrancefee : 0.01 ether,
-            interval: 30,
-            vrfcoodinator: ,
-            gasLane:  ,
+            entrancefee : 0.01 ether, //1e16
+            interval: 30, //30 seconds 
+            vrfcoodinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae ,
             subscriptionId: 0,
             cllbackGasLimit: 50000 
 
