9dd9ff2e9080fb3290792cc404fc3d2d8e1611cd
scofield <scofields109@gmail.com>
2024-11-27 05:55:11 +0100

    shifted for now

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,9 +1,17 @@
 //SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_Mock} from "chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_Mock.sol";
+
+abstract contract CodeConstants{
+    uint256 public constant ETH_SAPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
 
 
 contract HelperConfig is Script{
+    error HelperConfig__InvalidCHAIN_ID;
+
     struct NetworkConfig {
         uint256 entrancefee,
         uint256 interval,
@@ -16,9 +24,18 @@ contract HelperConfig is Script{
     mapping (uint256 chainId => NetworkConfig) public networkConfigs;
 
     constructor () {
-        
+        networkConfigs[ETH_SAPOLIA_CHAIN_ID] = getSapolia();
     }
 
+    function getConfigByChainId(uint256 chainId) public view returns(networkConfig memory){
+        if(networksConfigs[chainId].vrfCoordinator != address(0)){
+            return networkConfigs[chainId]
+        }else if (chainId = LOCAL_CHAIN_ID){
+            return getCreateAnvilETHconfig()
+        }else {
+            revert HelperConfig__InvalidCHAIN_ID()
+        }
+    }
     function getSapolia() public view returns (Networkconfig memory){
         return NetworkConfig({
             entrancefee : 0.01 ether, //1e16
@@ -29,6 +46,15 @@ contract HelperConfig is Script{
             cllbackGasLimit: 50000 
 
         });
+    }
+    function getCreateAnvilETHconfig() public returns (NetworkConfig memory){
+        if (localNetworkConfig.vrfcoodinator != address(0)){
+            return localNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        VRFCoodinatorV2_5Mock vrfCoodinatorMock = new VRFCoodinatorV2_5Mock(MOCK_BASE)
+        vm.stopBroadcast();
     }
 
 
